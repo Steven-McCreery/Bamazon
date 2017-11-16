f481c9d3a6c584a3c3aec2aec3828d7ee788a781
Steven McCreery <logad8@gmail.com>
2017-11-16 16:25:54 -0500

    got product selection working better, need to seperate connection from table display.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,10 @@
 
+// pulling in inquirer
 var inquirer = require("inquirer");
 
 // easy-table setup
 var Table = require("easy-table");
 var table = new Table;
-
-// cli-table setup
-// var Table = require("cli-table");
-// var table = new Table({
-// 	chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗', 'bottom': '═' , 'bottom-mid': '╧', 'bottom-left': '╚' , 'bottom-right': '╝', 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼', 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
-// });
 
 var mysql = require("mysql");
 
@@ -21,87 +16,146 @@ var connection = mysql.createConnection({
 	database: "bamazon_db"
 })
 
-var product = "";
-var department = "";
-var price = 0;
-var stock = 0;
+var productID = 0;
+var buyQty = 0;
 
-connection.connect(function(error) {
-	if (error) {
-		throw error;
-	} else{
-		console.log("================================================");
-		console.log("Welcome to Bamazon! Thanks for shopping with us!");
-		console.log("================================================");
+function roleSelect() {
+	inquirer.prompt([
+	{
+		type: "list",
+		message: "Please select your choice!",
+		choices: ["Buy an Item", "Enter Manager View", "Enter Supervisor View"],
+		name: "option"
+	}
+	]).then(function(choice){
+		switch (choice.option) {
+			case "Buy an Item":
+				console.log("Customer View");
+				customer.begin();
+				return;
+
+			case "Enter Manager View":
+				console.log("Manager View");
+				roleSelect();
+				return;
+
+			case "Enter Supervisor View":
+				console.log("Supervisor View");
+				roleSelect();
+				return;
+		}
+	});
+};
+
+var customer = {
+
+
+	begin() {
+		connection.end();
+		connection.connect(function(error) {
+			if (error) {
+				throw error;
+			} else{
+				console.log("================================================");
+				console.log("Welcome to Bamazon! Thanks for shopping with us!");
+				console.log("================================================");
+				connection.query("SELECT * FROM products", function(err, res) {
+					if (err) {
+						throw err;
+					} else {
+
+						// easy-table rendering method
+						res.forEach(function(t) {
+							table.cell('Product Id', t.item_id)
+							table.cell('Description', t.product_name)
+							table.cell('Department', t.department_name)
+							table.cell('Price, USD', t.price, Table.number(2))
+							table.cell('Available Qty', t.stock_quantity)
+							table.newRow()
+						});
+						console.log(table.toString());
+						customer.itemSelect();
+					}
+				});
+				// setTimeout()
+				
+				// connection.end();
+			}
+		})
+	},
+
+	itemSelect() {
+		inquirer.prompt([
+			{
+				type: "input",
+				message: "Please enter the Product ID Number of the item you would like to purchase.",
+				name: "productID",
+				validate: function(id) {
+					if (id.length < 1) {
+						console.log("Please select an item.");
+						return false;
+					} else if (id.length > 2) {
+						console.log("Please select a valid product.");
+						return false;
+					}
+					// else if () {}
+					else {
+					// return true;
+						
+						return true;
+					}
+				}
+			},
+			{
+				type: "Input",
+				message: "Please select the quantity you would like to purchase.",
+				name: "purchaseQty",
+				// validate: function(amount) {
+				// 	if (amount > ) {}
+				// }
+			}
+		]).then(function(buy) {
+			productID = buy.productID;
+			console.log(productID);
+			buyQty = buy.purchaseQty;
+			customer.transact();
+		})
+	},
+
+	transact() {
+		console.log("transact function");
 		connection.query("SELECT * FROM products", function(err, res) {
 			if (err) {
 				throw err;
 			} else {
-
-				// easy table method
-				res.forEach(function(t) {
-					table.cell('Product Id', t.item_id)
-					table.cell('Description', t.product_name)
-					table.cell('Department', t.department_name)
-					table.cell('Price, USD', t.price)
-					table.cell('Available Qty', t.stock_quantity, Table.number(2))
-					table.newRow()
-				});
-				console.log(table.toString());
-
-				// cli-table method
-				// table.push(res);
-				// console.log(table.toString());
+				console.log("transaction complete");
+				customer.again();
 			}
-		});
-		customer();
-		// connection.end();
-	}
-})
+		})
+	},
 
-function customer() {
-	console.log("customer function");
-	// inquirer.prompt([
-	// 	{
-
-	// 	}	
-	// ]).then(function(buy) {
-	// 	product = buy.product;
-	// 	department = buy.department;
-	// 	price = buy.price;
-	// 	stock = buy.stock;
-	// 	transact();
-	// })
-
-	transact();
+	again () {
+		console.log("again function");
+		inquirer.prompt([
+			{
+				type: "confirm",
+				message: "Would you like to make another purchase?",
+				name: "productID",
+				default: true
+			}
+		]).then(function(buy) {
+			if (buy.productID) {
+				connection.end();
+				roleSelect();
+			} else {
+				console.log("Thanks for visiting!");
+				connection.end();
+			}
+		})
+	},
 }
 
-function transact() {
-	console.log("transact function");
-	connection.query("SELECT * FROM products", function(err, res) {
-		if (err) {
-			throw err;
-		} else {
-			console.log("transaction complete");
-		}
-	})
-	again();
-}
-
-function again () {
-	console.log("again function");
-	connection.end();
-	// inqurier.prompt([
-	// 	{
-
-	// 	}
-	// ])
-}
-
-
-
-
-
+roleSelect();
 
 
 
